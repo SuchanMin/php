b51217c5de40372d282706183405a39562b45be9
SuchanMin <suchanwongsakun@gmail.com>
2025-11-20 09:49:40 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Hello</title>
+</head>
+<body>
+   <?php
+   echo " My first PHP script !";
+   echo "<bro>";
+   echo "<h1> My Name Suchanchira "; 
+   ?>
+</body>
+</html>
